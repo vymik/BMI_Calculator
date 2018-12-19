84433714107bb289b0930a33cdaa44f275cd43e2
vymik <43873650+vymik@users.noreply.github.com>
2018-12-19 18:34:04 +0200

    added js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,83 @@
 "use strict";
 
 
-  
+let height = document.querySelector('#height-input');
+let weight = document.querySelector('#weight-input');
+let buttons = document.querySelectorAll('button');
+let heightImperialButton = document.querySelector('#heightImperialButton');
+let weightImperialButton = document.querySelector('#weightImperialButton');
+let heightMetricButton = document.querySelector('#heightMetricButton');
+let weightMetricButton = document.querySelector('#weightMetricButton');
+let heightLabel = document.querySelector('#height-label');
+let weightLabel = document.querySelector('#weight-label');
 
+
+// buttons.forEach( button => {
+//     button.addEventListener('click', () => {
+//         button.classList.toggle('active');
+//     })
+// });
+
+heightImperialButton.addEventListener('click', () => {
+    heightImperialButton.classList.toggle('active');
+    heightMetricButton.classList.toggle('active');
+    changeHeightLabelText();
+    convertHeightInputValue();
+})
+
+heightMetricButton.addEventListener('click', () => {
+    heightImperialButton.classList.toggle('active');
+    heightMetricButton.classList.toggle('active');
+    changeHeightLabelText();
+    convertHeightInputValue();
+})
+
+weightMetricButton.addEventListener('click', () => {
+    weightImperialButton.classList.toggle('active');
+    weightMetricButton.classList.toggle('active');
+    changeWeightLabelText();
+    convertWeightInputValue();
+})
+
+weightImperialButton.addEventListener('click', () => {
+    weightImperialButton.classList.toggle('active');
+    weightMetricButton.classList.toggle('active');
+    changeWeightLabelText();
+    convertWeightInputValue();
+})
+
+let changeHeightLabelText = () => {
+    if(heightMetricButton.className == 'active'){
+        heightLabel.textContent = 'cm';
+    } else {
+        heightLabel.textContent = 'inch';
+    }
+}
+
+let changeWeightLabelText = () => {
+    if(weightMetricButton.className == 'active'){
+        weightLabel.textContent = 'kg';
+    } else {
+        weightLabel.textContent = 'lb';
+    }
+}
+
+let convertHeightInputValue = () => {
+    if(height.value != ''){
+        if(heightMetricButton.className != 'active'){
+            height.value =  (Number(height.value) / 2.54).toFixed(2);  
+        } else {
+            height.value =  (Number(height.value) * 2.54).toFixed(2);
+        }
+    }
+}
+
+let convertWeightInputValue = () => {
+    if(weight.value != ''){
+        if(weightMetricButton.className != 'active'){
+            weight.value =  (Number(weight.value) / 0.453592).toFixed(2);  
+        } else {
+            weight.value =  (Number(weight.value) * 0.453592 ).toFixed(2);
+        }
+    }
+}
